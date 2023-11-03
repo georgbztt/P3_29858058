d0a88cd51454df0eade3c9e8b9b0569705fca133
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2023-11-03 15:05:36 -0500

    feat

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const routes = require("./routes");
+
+app.use("/", routes);
+
+app.use(bodyParser.json());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+
+// Configura EJS como motor de plantillas
+app.set("view engine", "ejs");
+
+app.set("views", __dirname + "/views");
+
+const port = 3000;
+// Inicia el servidor
+app.listen(port, () => {
+    console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
